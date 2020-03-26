6f3424644fef12288caebcd25f1d0baa3458bbb8
Johnnie Birch <45402135+jlb6740@users.noreply.github.com>
2020-03-26 10:27:41 -0700

    Guard rust bindings search on windows
    On windows platform there is a guard against building the rust
    bindings for ittapi but later on we look for the library anyway.
    This patch guards against that second search on windows.

--- ittapi-rs/src/lib.rs
+++ ittapi-rs/src/lib.rs
@@ -1,5 +1,7 @@
 
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 #![allow(unused)]
+#[cfg(not(target_os = "windows"))]
 include!(concat!(env!("OUT_DIR"), "/ittnotify_bindings.rs"));
+#[cfg(not(target_os = "windows"))]
 include!(concat!(env!("OUT_DIR"), "/jitprofiling_bindings.rs"));
